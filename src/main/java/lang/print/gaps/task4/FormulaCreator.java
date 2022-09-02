6f982bcb7afbce159ebcf4484914590d1a174120
Sherzod <sherzodd7707@gmail.com>
2022-09-02 09:09:24 +0500

    I learned how to work with primitives and work on my mistakes

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,13 +1,11 @@
 package lang.print.gaps.task4;
 
-import java.util.Scanner;
 
 public class FormulaCreator {
-    Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
-       int a= 10;
-       int b=12;
-       int answer=((9*(a*a)-(5*b)+2+a-7)*((a+b-(4*a*b))/2));
+       int a= 1;
+       int b=1;
+       double answer= (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(answer);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,13 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c= 'c';
-        char h= 'h';
-        char a= 'a';
-        char r= 'r';
-        System.out.println(c);
-        System.out.println(h);
-        System.out.println(a);
-        System.out.println(r);
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r= 114;
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
+
     }
 }
